e8438d34f3d39b42078e4091e26b34e9d72808ea
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2023-09-19 05:01:49 -0600

    Deleting logic to reformat

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,29 +3,10 @@ import { useState,useEffect } from "react"
 
 const LandingPage = () => {
     
-    const [info,setInfo] = useState([])
-
-    useEffect(()=> {axios.get(`https://rickandmortyapi.com/api/character`).then(resp =>{
-        console.log("Resp: ",resp.data.results)
-        setInfo(resp.data.results)
-    })},[]) 
-
-    //console.log("LandingComponent: ",keyWord)
-
-    const traerInformacion = () =>{
-        return info.map((personaje) => (
-            <div key={personaje.id}>
-                <img src={personaje.image} alt="" />
-                <p>{personaje.name}</p>
-            </div>
-        ))
-    }
 
     return (
         <>
-        {
-            traerInformacion()
-        }
+        <p>Hola mundo</p>
         </>
     )
 }
