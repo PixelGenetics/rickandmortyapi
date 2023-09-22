b5fc8ebced8acd2dd5c9a20f9218f941b19be335
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2023-09-21 18:56:43 -0600

    Renaming LandingPage Item to Menu.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import './App.css'
 import NavBar from './components/NavBar'
 import SearchBar from './components/SearchBar'
-import LandingPage from './components/LandingPage'
 
 function App() {
 
