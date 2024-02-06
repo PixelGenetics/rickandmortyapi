291e51d6163b30d69c6bb56a73c0ed16703fa13f
Kevin Antonio Borge Gutierrez <108099077+PixelGenetics@users.noreply.github.com>
2024-02-06 00:11:02 -0600

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import { useState } from 'react'
 import './App.css'
 import NavBar from './components/NavBar'
 import SearchBar from './components/SearchBar'
+import ByLocation from './components/ByLocation'
 
 function App() {
 
 
   return (
-    <>
+    <div className='bg-[#023047]'>
       <NavBar />
-      <SearchBar />
-    </>
+      {/* <SearchBar /> */}
+      <ByLocation />
+    </div>
   )
 }
 
